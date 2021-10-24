d9010636a07f687619d36f9b4e7d08af3841aa90
SandeshSubramanya <91111862+SandeshSubramanya@users.noreply.github.com>
2021-10-24 23:16:31 +0530

    Adapt stats.cpp to handle testcase 1 and 2

--- stats.cpp
+++ stats.cpp
@@ -1,5 +1,31 @@
 #include "stats.h"
+#include <algorithm>
+#include <cmath>
 
-Stats Statistics::ComputeStatistics(const std::vector<___>& ) {
-    //Implement statistics here
+Statistics::Stats Statistics::ComputeStatistics(const std::vector<double>& rfVectorValues) 
+{
+   Statistics::Stats oTempStatistics;
+   double sum = 0.0;
+   if (!rfVectorValues.empty())
+   {
+      // compute statistics with the input vector values.
+      std::vector<double>::const_iterator vectorValuesItr = rfVectorValues.begin();
+      for (; vectorValuesItr != rfVectorValues.end(); ++vectorValuesItr)
+      {
+         sum += *vectorValuesItr;
+      }
+
+      oTempStatistics.average = (double) (sum / (double)rfVectorValues.size());
+      oTempStatistics.max = *std::max_element(rfVectorValues.begin(), rfVectorValues.end());
+      oTempStatistics.min = *std::min_element(rfVectorValues.begin(), rfVectorValues.end());
+   }
+   else
+   {
+      // vector is empty. Assign "nan" to all the values.
+      oTempStatistics.average = (double)std::nan("1");
+      oTempStatistics.max = (double)std::nan("1");
+      oTempStatistics.min = (double)std::nan("1");
+   }
+    
+   return oTempStatistics;
 }
